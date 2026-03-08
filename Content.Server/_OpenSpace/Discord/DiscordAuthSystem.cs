b4a0e480fdd9cb16a67ac13413b802edf811d3b5
Rinary <rinary.super@gmail.com>
2026-03-08 17:27:10 +0200

    request roles on shared

--- Content.Server/_OpenSpace/Discord/DiscordAuthSystem.cs
+++ Content.Server/_OpenSpace/Discord/DiscordAuthSystem.cs
@@ -1,8 +1,11 @@
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using Content.Server.GameTicking;
+using Content.Shared._OpenSpace.Discord;
 
 namespace Content.Server._OpenSpace.Discord;
 
-public sealed class DiscordAuthSystem : EntitySystem
+public sealed class DiscordAuthSystem : SharedDiscordAuthSystem
 {
     [Dependency] private readonly IDiscordOAuthManager _discordOAuth = default!;
     public override void Initialize()
@@ -10,10 +13,30 @@ public sealed class DiscordAuthSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<PlayerConnectEvent>(OnPlayerConnection);
+        SubscribeNetworkEvent<RequestDiscordRoles>(OnRolesRequest);
+    }
+
+    ConcurrentDictionary<string, HashSet<ulong>> _roles = [];
+
+    private async void OnRolesRequest(RequestDiscordRoles args)
+    {
+        if (_roles.TryGetValue(args.Uuid, out var roles) && roles.Count > 0)
+            args.Roles = roles;
+        else
+            args.Roles = await RequestRoles(args.Uuid);
     }
 
     private async void OnPlayerConnection(PlayerConnectEvent args)
     {
-        await _discordOAuth.GetRoles(args.PlayerSession.UserId.ToString());
+        var uuid = args.PlayerSession.UserId.ToString();
+        await RequestRoles(uuid);
+    }
+
+    private async Task<HashSet<ulong>> RequestRoles(string uuid)
+    {
+        var playerRoles = await _discordOAuth.GetRoles(uuid);
+        _roles[uuid] = playerRoles;
+
+        return playerRoles;
     }
 }
